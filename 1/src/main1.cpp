ca8cd3198ee39c208527e363442ff451017c0897
arthur-kotaro <arthur.kotaro@yandex.ru>
2025-09-19 11:38:42 +0400

    task1 checking

--- 1/src/main1.cpp
+++ 1/src/main1.cpp
@@ -6,6 +6,7 @@
 enum Colour {red, green, blue};
 class GeomShape
 {
+protected:
     Colour shapeColour;
     double center_X_AxisCoordinate;
     double center_Y_AxisCoordinate;
@@ -20,19 +21,19 @@ public:
         std::cin >> center_X_AxisCoordinate >> center_Y_AxisCoordinate;
         do
         {
-            std::cout << "Enter colour (red,green, blue): ";
+            std::cout << "Enter colour (red, green, blue): ";
             std::cin >> inpColour;
         } while (inpColour != "red" && inpColour != "green" && inpColour != "blue");
         if(inpColour == "red") shapeColour = red;
         else if(inpColour == "green") shapeColour = green;
         else if(inpColour == "blue") shapeColour = blue;
     }
-    ShowInfo()
+    void ShowInfo()
     {
         if(shapeColour == red) std::cout << "Red ";
         else if(shapeColour == green) std::cout << "Green ";
         else if(shapeColour == blue) std::cout << "Blue ";
-        std::cout << input_str << "\'s area equals to " << tmp->FigureOutArea() << ". It\'s Circumscribed Rectangle\'s Sides equals to " << CircumscribedRectanglesSideA << " and " << CircumscribedRectanglesSideB << ".\n";
+        std::cout << "Shape\'s area equals to " << ShapeArea << ". It\'s Circumscribed Rectangle\'s Sides equals to " << CircumscribedRectanglesSideA << " and " << CircumscribedRectanglesSideB << ".\n";
 
     }
 };
@@ -54,7 +55,7 @@ public:
 
     double FigureOutArea() { return M_PI * radius * radius; }
 
-    FigureOutCircumscribedRectangle()
+    void FigureOutCircumscribedRectangle()
     {
         CircumscribedRectanglesSideA = 2 * radius;
         CircumscribedRectanglesSideB =  CircumscribedRectanglesSideA;
@@ -78,10 +79,10 @@ public:
 
     double FigureOutArea() { return squareSideLength * squareSideLength; }
 
-    FigureOutCircumscribedRectangle()
+    void FigureOutCircumscribedRectangle()
     {
-        CircumscribedPectanglesSideA = squareSideLength;
-        CircumscribedRectanglesSideB = CircumscribedPectanglesSideA;
+        CircumscribedRectanglesSideA = squareSideLength;
+        CircumscribedRectanglesSideB = CircumscribedRectanglesSideA;
     }
 };
 
@@ -102,22 +103,20 @@ public:
 
     double FigureOutArea() { return equilateralTriangleSideLength * equilateralTriangleSideLength * std::sqrt(3) / 4; }
 
-    FigureOutCircumscribedRectangle()
+    void FigureOutCircumscribedRectangle()
     {
-        CircumscribedPectanglesSideA = equilateralTriangleSideLength;
-        CircumscribedPectanglesSideB = CircumscribedPectanglesSideA * sin (M_PI / 6); // sin (90 - (180/3) = sin (30), 30 in rad equals 2Pi / (360/30) => 2Pi / 12 => Pi / 6
+        CircumscribedRectanglesSideA = equilateralTriangleSideLength;
+        CircumscribedRectanglesSideB = CircumscribedRectanglesSideA * sin (M_PI / 6); // sin (90 - (180/3) = sin (30), 30 in rad equals 2Pi / (360/30) => 2Pi / 12 => Pi / 6
     }
 };
 
 class Rectangle:public GeomShape
 {
-    double rectangle_A_SideLength;
-    double rectangle_B_SideLength;
+    double RectangleSide_A_Length;
+    double RectangleSide_B_Length;
 public:
     Rectangle()
     {
-        double RectangleSide_A_Length;
-        double RectangleSide_B_Length;
         do
         {
             std::cout << "Enter rectangles A-side length and B-side length : ";
@@ -129,10 +128,10 @@ public:
 
     double FigureOutArea() { return RectangleSide_A_Length * RectangleSide_B_Length;}
 
-    FigureOutCircumscribedRectangle()
+    void FigureOutCircumscribedRectangle()
     {
-        CircumscribedPectanglesSideA = RectangleSide_A_Length;
-        CircumscribedPectanglesSideB = RectangleSide_B_Length);
+        CircumscribedRectanglesSideA = RectangleSide_A_Length;
+        CircumscribedRectanglesSideB = RectangleSide_B_Length;
     }
 };
 
@@ -140,15 +139,32 @@ public:
 int main()
 {
     std::string input_str;
-    void * tmp = nullptr;
+//    void * tmp = nullptr;
+    GeomShape * tmp = nullptr;
     do
     {
         std::cout<< "\nEnter command (circle/square/triangle/rectangle/exit): ";
         std::cin>>input_str;
-        if (input_str == "circle")          (Circle *) tmp = new Circle;
-        else if (input_str == "square")     (Square *) tmp = new Square;
-        else if (input_str == "triangle")   (EquilateralTriangle *) tmp = new EquilateralTriangle;
-        else if (input_str == "rectangle")  (Rectangle * tmp) = new Rectangle;
+        if (input_str == "circle")
+        {
+            static_cast<Circle *> (tmp);
+            tmp = new Circle;
+        }
+        else if (input_str == "square")
+        {
+            static_cast<Square *> (tmp);
+            tmp = new Square;
+        }
+        else if (input_str == "triangle")
+        {
+            static_cast<EquilateralTriangle *> (tmp);
+            tmp = new EquilateralTriangle;
+        }
+        else if (input_str == "rectangle")
+        {
+            static_cast<Rectangle *> (tmp);
+            tmp = new Rectangle;
+        }
         else if (input_str == "exit")       break;
         else
         {
